2f6cace3d2148057b4ba6ed98d14fd3701760a29
Jonathan Ray <jonathanray17@gmail.com>
2019-04-04 12:04:28 -0400

    adds partials support

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
 // functions for res.render-ing user info from routes
 function getUserProfile(req, res){
-    res.render('user', {locals:{
-        username:"dummy Name",
-        password:"lol, not really"
-    }});
+    res.render('user', {
+        locals:{
+            username:"dummy Name",
+            password:"lol, not really"
+    },
+        partials:{
+            settings: './partial-settings'
+        }});
 }
 
 module.exports = {
