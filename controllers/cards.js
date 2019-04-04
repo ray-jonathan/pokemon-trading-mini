a164df4369885bcd3623203aa9a1c5456eea8545
Jonathan Ray <jonathanray17@gmail.com>
2019-04-04 16:24:32 -0400

    adds bootstrap cards for the pokemon cards and wires them through to a trade page (results)

---
+++ controllers/cards.js
@@ -0,0 +1,21 @@
+// functions for res.render-ing user info from routes
+const  Card = require('../models/cards');
+
+async function getCards(req, res){
+
+    const allTheCards = await Card.getAllCards();
+    // console.log(allTheCards);
+    res.render('cards', {
+        locals:{
+            username: "ash",
+            password: "pikachu",
+            Card: allTheCards
+    },
+        partials:{
+            bootstrap: './partial-settings'
+        }});
+}
+
+module.exports = {
+    getCards
+};
