b204347c256fcefd2496601d6733b62cb20efbfa
Jonathan Ray <jonathanray17@gmail.com>
2019-04-04 10:31:09 -0400

    cleaning up house before branching

--- testing/index.js
+++ testing/index.js
@@ -1,6 +1,15 @@
-const pokemon = require('pokemontcgsdk')
+const pokemon = require('pokemontcgsdk');
 
-pokemon.card.find('base1-1')
-.then(result => {
-    console.log(result.card.name) // "Charizard"
-})
+console.log("hello 1?");
+pokemon.card.all({ setCode: 'base1', pageSize: 1 })
+.on('data', card => {
+    console.log(card);
+});
+console.log("hello 2?");
+
+
+
+// pokemon.card.all({ set: 'base', name: 'charmander', pageSize: 1 })
+// .on('data', card => {
+//     console.log(card)
+// })
