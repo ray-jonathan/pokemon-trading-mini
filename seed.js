5df3960cdcc904b01bfd0b738f3fedc046db6250
Matt Raines <mraines4@gmail.com>
2019-04-05 08:39:36 -0400

    loosly encrypts seed data

--- seed.js
+++ seed.js
@@ -6,3 +6,36 @@ allCards.cards.forEach ((oneCard) => {
     Card.add(oneCard);
 })
 
+const User = require('./models/users')
+
+async function encryptSeed1() {
+    const user = await User.getByUsername('AshTheVeryBest');
+    user.setPassword('PikachuRox');
+    await user.save();
+    // console.log('you did the thing')
+}
+encryptSeed1();
+
+async function encryptSeed2() {
+    const user = await User.getByUsername('Mistywater99');
+    user.setPassword('BlubBlubBlub');
+    await user.save();
+    // console.log('you did the thing')
+}
+encryptSeed2();
+
+async function encryptSeed3() {
+    const user = await User.getByUsername('BrockTheBoss');
+    user.setPassword('R0ckS0lid');
+    await user.save();
+    // console.log('you did the thing')
+}
+encryptSeed3();
+
+async function encryptSeed4() {
+    const user = await User.getByUsername('Professor_oak');
+    user.setPassword('Ch00seUrFate');
+    await user.save();
+    // console.log('you did the thing')
+}
+encryptSeed4();
